6ea0bfd4b01476e84926a8d1d3cae37d0c41a44e
anikmondol <anikmondol558363@gmil.com>
2024-03-15 18:50:00 +0600

    about as array in this video

---
+++ array.js
@@ -0,0 +1,14 @@
+const num1 = 12;
+const num2 = 22;
+const num3 = 441;
+const num4 = 454;
+
+
+// first:(), seconde:{}, third:[] 
+
+// const numbers = 132132131321;
+
+const numbers = [10, 20, 12, 30];
+const friends = ['anik', 'apu', 'arpon'];
+const passed = [true, false,true];
+const mixedArray = [12, 'address', true, 12.07]
